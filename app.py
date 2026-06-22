f891a33bdb002eb5109f714556484bd3d3c0f9a6
annocuoi <92977855+annocuoi@users.noreply.github.com>
2026-06-22 18:11:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -542,22 +542,35 @@ if not st.session_state.da_dang_nhap:
 
             for ten_hoi, info in st.session_state.tai_khoan.items():
 
-                if info.get("quyen") == "hoi":
+                if info.get("quyen") != "hoi":
+                    continue
+
+
+                try:
 
                     data_hoi = doc_du_lieu_hoi(ten_hoi)
 
-                    tk_xem = data_hoi.get("_tai_khoan_xem", {})
+                except:
 
-                    if (
-                        ten_dang_nhap == tk_xem.get("user")
-                        and mat_khau_nhap == tk_xem.get("pass")
-                    ):
+                    continue
 
-                        dang_nhap_ok = True
-                        quyen_login = "xem"
-                        chu_so_huu = ten_hoi
 
-                        break
+                tk_xem = data_hoi.get(
+                    "_tai_khoan_xem",
+                    {}
+                )
+
+
+                if (
+                    ten_dang_nhap == tk_xem.get("user")
+                    and mat_khau_nhap == tk_xem.get("pass")
+                ):
+
+                    dang_nhap_ok = True
+                    quyen_login = "xem"
+                    chu_so_huu = ten_hoi
+
+                    break
 
 
         if dang_nhap_ok:
